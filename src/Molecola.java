bef6262d5c94f268b65524352f72b77047ffd6e3
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2025-10-21 10:12:49 +0200

    Feat: update class Molecola

--- src/Molecola.java
+++ src/Molecola.java
@@ -1,7 +1,31 @@
 
+import java.util.ArrayList;
+
+
 public class Molecola {
     private String nome;
     private boolean ione;
     private Composto composto;
     private ArrayList<Atomo> atomi;
+
+    public Molecola(String nome, boolean ione, Composto composto, ArrayList<Atomo> atomi) {
+        this.nome = nome;
+        this.ione = ione;
+        this.composto = composto;
+        this.atomi = atomi;
+    }
+
+    public double getPesoMolecolare() {
+        double somma = 0.0;
+        for (Atomo a : atomi) {
+            somma += a.getPesoAtomico();
+        }
+        return somma;
+    }
+
+    public void impostaIone() {
+        
+    }
+
+    
 }
